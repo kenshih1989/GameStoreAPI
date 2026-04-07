c7234fdab75e9485c40a26235ffdc0c2f64c2224
Gan Leng Keong <kenshih_keong@hotmail.com>
2026-04-07 23:41:25 +0800

    Work until entity framework core integration

--- GameStore.Api/GameStore.Api/Program.cs
+++ GameStore.Api/GameStore.Api/Program.cs
@@ -1,6 +1,14 @@
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddValidation();
+
+var connectionString = "DataSource=GameStore.db";
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
+
+// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 var app = builder.Build();
 
 app.MapGamesEndpoints();
--- GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/GameStore.Api/obj/Debug/net10.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a647f45288355ed6a9b74faccb9abb6135cb0d7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c872d4d9ccf28bec2a6fdb4b858ef4bca564317")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
